d2ece4a93cb6b028ce5f3ff23d7d4d65604d5ddc
Carlos Meza <carlos@digitalr00ts.com>
2022-09-08 15:02:39 -0700

    feat: add bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+PLUGIN_PATH='User/globalStorage/zokugun.sync-settings'
+
+OS="$(uname)"
+case "${OS}" in
+#   'Linux')
+#     ;;
+  'Darwin')
+    PLUGIN_SETTINGS="${HOME}/Library/Application Support/VSCodium/${PLUGIN_PATH}"
+    ;;
+  *)
+    printf -- "ERROR: ${OS} support not implemented."
+    exit 1
+    ;;
+esac
+
+codium --install-extension zokugun.sync-settings
+mkdir -p ${PLUGIN_SETTINGS}
+git clone --depth 1 git@github.com:xsc27/settings-codium.git "${PLUGIN_SETTINGS}/repository"
+cp "${PLUGIN_SETTINGS}/repository/settings.yml" "${PLUGIN_SETTINGS}"
